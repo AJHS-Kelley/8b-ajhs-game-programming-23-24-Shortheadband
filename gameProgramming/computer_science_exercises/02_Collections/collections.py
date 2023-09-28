5e3f10cc6e40975c206bb22ddc97ef1a7c11669c
Shortheadband <142345256+Shortheadband@users.noreply.github.com>
2023-09-28 14:17:28 -0400

    v0,1a
    Declared and defined three lists

--- gameProgramming/computer_science_exercises/02_Collections/collections.py
+++ gameProgramming/computer_science_exercises/02_Collections/collections.py
@@ -1,1 +1,13 @@
 #Collections Examples, Ryan Kelley, v0.0
+
+# LIST -- ORDERED, CHANGEABLE, ALLOWS CUPLICATE VALUES
+breakfastfoods = ["Bacon","Waffles","Pancakes", "Cereal", "Milk"]
+# Each item on the list known as an ELEMENT.
+# The position in the list for each is the INDEX
+# The element "BACON" is at index 0.
+# Python Only: index -1 it is the last item on the list
+testScores = [95,100,25,15,27,35]
+classGPA = [3.14, 2.25, 1.74, 1.99, 0.99, 4.25]
+
+
+
