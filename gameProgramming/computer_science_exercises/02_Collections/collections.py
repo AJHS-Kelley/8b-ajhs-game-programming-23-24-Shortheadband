fc05cfc9fdb162b9c62d058886fe74f42afed55f
Shortheadband <142345256+Shortheadband@users.noreply.github.com>
2023-10-02 14:03:33 -0400

    v0.2b
    Using .insert() and .append() to add items to lists.

--- gameProgramming/computer_science_exercises/02_Collections/collections.py
+++ gameProgramming/computer_science_exercises/02_Collections/collections.py
@@ -41,9 +41,35 @@ classGPA = [3.14, 2.25, 1.74, 1.99, 0.99, 4.25]
 #print(classGPA)
 
 # Pause -- WYOC -- Change 5th Element
-breakfastfoods[4] = "Bagel"
-testScores[4] = 45
-classGPA[4] = 2.45
+#breakfastfoods[4] = "Bagel"
+#testScores[4] = 45
+#classGPA[4] = 2.45
+#print(breakfastfoods)
+#print(testScores)
+#print(classGPA)
+
+# Adding and Inserting Items to a List
+# .append() adds an item to the end
+#breakfastfoods.append("hash browns")
+#print(breakfastfoods)
+#testScores.append(99)
+#print(testScores)
+#classGPA.append(1.99)
+#print(classGPA)
+
+# .insert() allows you to place an item at a specfic index in the list
+#breakfastfoods.insert(3, "parfait")
+#print(breakfastfoods)
+#testScores.Insert(3, 55)
+#print(testScores)
+#classGPA.Insert(3, 0.0)
+#print(classGPA)
+
+# Pause -- WYOC -- .apprnd() another item to each list. .insert() an item at index 5 to each list.
+breakfastfoods.append("Honey Bun")
 print(breakfastfoods)
+testScores.append(99)
 print(testScores)
+classGPA.append(1.99)
 print(classGPA)
+
