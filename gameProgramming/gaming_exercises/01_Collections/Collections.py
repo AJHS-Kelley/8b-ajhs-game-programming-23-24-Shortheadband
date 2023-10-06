891ba417644d19d51fc40c4583a081e01780ea38
Shortheadband <142345256+Shortheadband@users.noreply.github.com>
2023-10-06 10:02:45 -0400

    Collections GA v0.1
    Added functional player inventory.

--- gameProgramming/gaming_exercises/01_Collections/Collections.py
+++ gameProgramming/gaming_exercises/01_Collections/Collections.py
@@ -1,22 +1,28 @@
+#Collections GA v0.1
+
 # Adding items
 
 playerInventory = []
 
 i = 1
 while i <= 10:
-    item = input("Add item .\n")
-    playerInventory.append(item)
+    item = input(f"Add item {i} .\n")
+    playerInventory.insert(i,item)
     i += 1
-
+    
 playerInventory.sort()
 print(f"The player's inventory is {playerInventory}.")
 
 # Removing items
 
-#i = 1
-#while 1 >= 5:
- #   print(i)
-  #  i -= 1
+i = 2
+while 2 >= -3:
+   itemRemove = int(input(f"To remove an item input the NUMBER tied to it.\n"))
+   isCorrect = input("Please type yes if correct, no if not correct.\n")
+   if isCorrect == "yes":
+    print(f"Ok, {playerInventory[itemRemove]} has been removed")
+   playerInventory.pop(itemRemove)
+   i -= 1
 
-#playerInventory.sort()
-#print(f"The player's inventory is {playerInventory}.")
+playerInventory.sort()
+print(f"The player's inventory is {playerInventory}.")
