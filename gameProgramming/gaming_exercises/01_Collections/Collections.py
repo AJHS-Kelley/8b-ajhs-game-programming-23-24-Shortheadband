174736facd44694e760bf775ef87aad794ce5ee1
Shortheadband <142345256+Shortheadband@users.noreply.github.com>
2023-10-06 11:53:40 -0400

    Collections GA v0.2
    Gave some QOF updates to adding and removing items removing items.

--- gameProgramming/gaming_exercises/01_Collections/Collections.py
+++ gameProgramming/gaming_exercises/01_Collections/Collections.py
@@ -1,28 +1,32 @@
-#Collections GA v0.1
+#Collections GA v0.2
 
 # Adding items
 
-playerInventory = []
+#playerInventory = []
 
-i = 1
-while i <= 10:
-    item = input(f"Add item {i} .\n")
-    playerInventory.insert(i,item)
-    i += 1
+#i = 0
+#while i <= 9:
+    #item = input(f"Add item {i} to your inventory .\n")
+    #playerInventory.insert(i,item)
+    #print(f"{item} has been added to your inventory.\n")
+    #i = (i + 1)
     
-playerInventory.sort()
-print(f"The player's inventory is {playerInventory}.")
+#playerInventory.sort()
+#print(f"The player's inventory is {playerInventory}.")
 
 # Removing items
 
-i = 2
-while 2 >= -3:
-   itemRemove = int(input(f"To remove an item input the NUMBER tied to it.\n"))
-   isCorrect = input("Please type yes if correct, no if not correct.\n")
-   if isCorrect == "yes":
-    print(f"Ok, {playerInventory[itemRemove]} has been removed")
-   playerInventory.pop(itemRemove)
-   i -= 1
+#i = 2
+#while 2 >= -3:
+   #itemRemove = int(input(f"To remove an item input the NUMBER tied to it.\n"))
+   #isCorrect = input("Please type yes if correct, no if not correct.\n")
+   #if isCorrect == "yes":
+    #print(f"Ok, {playerInventory[itemRemove]} has been removed")
+    #playerInventory.pop(itemRemove)
+    #i -= 1
+   #else:
+        #print("Ok let's try that again.\n")
+        #i = i
 
-playerInventory.sort()
-print(f"The player's inventory is {playerInventory}.")
+#layerInventory.sort()
+#print(f"The player's inventory is {playerInventory}.")
