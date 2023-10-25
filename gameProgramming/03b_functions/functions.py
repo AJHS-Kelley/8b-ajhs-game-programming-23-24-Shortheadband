ed9dbd715c7fb22d2c6c9529ca953e979302afef
Shortheadband <142345256+Shortheadband@users.noreply.github.com>
2023-10-25 13:10:16 -0400

    functions update
    Added more to rolldice

--- gameProgramming/03b_functions/functions.py
+++ gameProgramming/03b_functions/functions.py
@@ -18,14 +18,39 @@ def exampleFunctionB(num, count): # PARAMETERS
 #exampleFunctionA()
 #exampleFunctionB(5,0)
 
-def rollDice(numDice, sizeDice:)
+def rollDice(numDice, sizeDice):
     numRolled = 0
-    while numRolled < numDice
+    while numRolled < numDice:
             roll = random.radint(1, sizeDice)
-               
-               
+            sum += roll
+            print(f"Roll: {roll}\n")
+            print(f"Sum: {sum}\n")
+            numRolled += 1
+        return sum
 
+#rolldice(3,6)
+#rolldice(1,20)
 
+strentghPlayer = rollDice(3,6)
+dexterityPlayer = rollDice(3,6)
+wisdomPlayer = rollDice(3,6)
 
+print
 
+def genstats():
+    playerstats = [
+         0, #Str
+         0, #Dex
+         0, #Con
+         0, #Int
+         0, #Wis
+         0  #Cha
+    ]
+    i = 0
+    while i < len(playerstats):
+        playerstats[i] = rollDice(3, 6) # strength
+        i += 1
 
+    print("playerStats")
+
+genstats()
