88f5c97bebf9595c82dfb087e86673785b436dad
Shortheadband <142345256+Shortheadband@users.noreply.github.com>
2023-10-31 14:18:28 -0400

    Hangman Game by Gabriel Coffey, v0.2
    Added images

--- gameProgramming/04b_Hangman/Hangman.py
+++ gameProgramming/04b_Hangman/Hangman.py
@@ -1,4 +1,56 @@
-# Hangman Game by Gabriel Coffey, vo.1
+# Hangman Game by Gabriel Coffey, vo.2
 
 words = 'Cat Bat Rat Car Bar Star Bored Help Belt Tilt Machine Hammer Hourglass Vogage Punishment Divine Rewind Combine TombStone Chrome Celestial Beatiful Guardian Alliance Lyric circular motherboard Critical Mystical Dynasty'.split()
 print(words)
+HANGMAN_BOARD = ['''
+   
+    +---+
+        |         
+        |         
+        |         
+     ========''', '''            
+              
+                 
+    +---+
+    0   |         
+        |         
+        |         
+     ========''', '''            
+
+    +---+
+    0   |         
+    |   |         
+        |         
+     ========''', '''            
+
+
+    +---+
+    0   |         
+   /|   |         
+        |         
+     ========''', '''            
+
+
+    +---+
+    0   |         
+   /|\  |         
+        |         
+     ========''', '''            
+
+
+    +---+
+    0   |         
+   /|\  |         
+   /    |         
+     ========''', '''            
+
+    +---+
+    0   |         
+   /|\  |         
+   / \  |         
+     ========''']
+
+#i = 0
+#while i < len(HANGMANBOARD):
+#    print(HANGMAN_BOARD[I])
+#    i += 1
