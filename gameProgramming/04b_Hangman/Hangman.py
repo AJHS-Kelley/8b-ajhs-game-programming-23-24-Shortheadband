961e815795dd5b0f1b814a34be214b85f324c23d
Shortheadband <142345256+Shortheadband@users.noreply.github.com>
2023-10-31 14:00:26 -0400

    Hangman Game by Gabriel Coffey, vo.1
    Added list

--- gameProgramming/04b_Hangman/Hangman.py
+++ gameProgramming/04b_Hangman/Hangman.py
@@ -1,1 +1,4 @@
-# Hangman Game by Gabriel Coffey, vo.o
+# Hangman Game by Gabriel Coffey, vo.1
+
+words = 'Cat Bat Rat Car Bar Star Bored Help Belt Tilt Machine Hammer Hourglass Vogage Punishment Divine Rewind Combine TombStone Chrome Celestial Beatiful Guardian Alliance Lyric circular motherboard Critical Mystical Dynasty'.split()
+print(words)
