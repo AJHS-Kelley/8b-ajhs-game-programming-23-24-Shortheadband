d909f4f2beab4d35651299692fb60c24dd73954f
Shortheadband <142345256+Shortheadband@users.noreply.github.com>
2023-10-31 14:34:00 -0400

    Emg update
    Added a function.(laptop almost dead)

--- gameProgramming/04b_Hangman/Hangman.py
+++ gameProgramming/04b_Hangman/Hangman.py
@@ -1,5 +1,7 @@
 # Hangman Game by Gabriel Coffey, vo.2
 
+import random
+
 words = 'Cat Bat Rat Car Bar Star Bored Help Belt Tilt Machine Hammer Hourglass Vogage Punishment Divine Rewind Combine TombStone Chrome Celestial Beatiful Guardian Alliance Lyric circular motherboard Critical Mystical Dynasty'.split()
 print(words)
 HANGMAN_BOARD = ['''
@@ -54,3 +56,15 @@ HANGMAN_BOARD = ['''
 #while i < len(HANGMANBOARD):
 #    print(HANGMAN_BOARD[I])
 #    i += 1
+
+# Pick Word from List
+def getRandomWord(wordList): # Return a random word from the list
+    wordIndex = random.randint(0,len(words) - 1)
+    # len(listName) - 1 is EXTREMELY COMMON FOR WORKING WITH LISTS.
+    return wordList[wordIndex]
+
+i = 0
+while i < 50:
+    word = getRandomWord(words)
+    print(word)
+    i += 1
