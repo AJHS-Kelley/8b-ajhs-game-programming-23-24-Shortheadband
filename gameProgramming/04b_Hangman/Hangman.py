f330513423e578c66452bec138b4e431676959ce
Shortheadband <142345256+Shortheadband@users.noreply.github.com>
2023-11-02 13:31:25 -0400

    Hangman Update
    added more code

--- gameProgramming/04b_Hangman/Hangman.py
+++ gameProgramming/04b_Hangman/Hangman.py
@@ -63,8 +63,12 @@ def getRandomWord(wordList): # Return a random word from the list
     # len(listName) - 1 is EXTREMELY COMMON FOR WORKING WITH LISTS.
     return wordList[wordIndex]
 
-i = 0
-while i < 50:
-    word = getRandomWord(words)
-    print(word)
-    i += 1
+def displayBoard(missedLetters, correctLetters , secretWord):
+    print(HANGMAN_BOARD[len(missedLetters)])
+
+#i = 0
+#while i < 50:
+#    word = getRandomWord(words)
+#    print(word)
+#    i += 1
+
