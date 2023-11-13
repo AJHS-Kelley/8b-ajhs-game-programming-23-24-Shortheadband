5b648f6d6f7df5ca8000e679962a97f11375bf26
Shortheadband <142345256+Shortheadband@users.noreply.github.com>
2023-11-13 14:41:29 -0500

    Game Function project, Gabriel Coffey, v0.1
    Added sprint function

--- gameProgramming/5a_exampleGameFunctions/exampleGameFunctions.py
+++ gameProgramming/5a_exampleGameFunctions/exampleGameFunctions.py
@@ -1,4 +1,4 @@
-# Example Game Function project, Gabriel Coffey, v0.0
+# Example Game Function project, Gabriel Coffey, v0.1
 import random
 def functionOne():
     pass
@@ -11,3 +11,29 @@ def functionThree(param1 = "Default Value"):
 
 def functionFour(param1, Param2, param3):
     pass
+
+# def catchBall(throwQuality, passCatchScore, weather):
+#    if throwQuality > 5.0 and passCatchScore >=99 and weather == 'sunny':
+#        ballcaught = True
+#    elif throwQuality > 4.0 and passCatchScore >= 75 and weather == 'Windy':
+#        ballcaught = False
+#    else:
+#        print('Oh, no, interception!\n')
+#        ballIntercepted = True
+#        return ballIntercepted
+#    return ballcaught
+
+#catchBall(4.25, 107, 'Rainy')
+
+# Game Type: Platformer
+
+def sprint(fullSprint, playerStamina, exhausted):
+    if playerStamina >= 1 is True and exhausted is False:
+        fullSprint = True
+    else: exhausted is True
+    
+
+def gameOver(lives,currency):
+    if
+
+    
