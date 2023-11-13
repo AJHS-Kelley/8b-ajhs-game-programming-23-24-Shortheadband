5a653c1d4c00ffc2e88a29a79853a06bc8273dad
Shortheadband <142345256+Shortheadband@users.noreply.github.com>
2023-11-13 13:51:41 -0500

    Ver update
    added 4 examples.

--- gameProgramming/5a_exampleGameFunctions/exampleGameFunctions.py
+++ gameProgramming/5a_exampleGameFunctions/exampleGameFunctions.py
@@ -1,1 +1,13 @@
 # Example Game Function project, Gabriel Coffey, v0.0
+import random
+def functionOne():
+    pass
+
+def functionTwo(param1):
+    pass
+    
+def functionThree(param1 = "Default Value"):
+    pass    
+
+def functionFour(param1, Param2, param3):
+    pass
