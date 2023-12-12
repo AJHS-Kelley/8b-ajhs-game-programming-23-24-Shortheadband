7c30ff2cc32f4eebcf403aec3df3f785a6f6f740
Shortheadband <142345256+Shortheadband@users.noreply.github.com>
2023-12-12 13:56:48 -0500

    Update oop.py
    added for to make people

--- gameProgramming/06_OOP/oop.py
+++ gameProgramming/06_OOP/oop.py
@@ -1,4 +1,16 @@
 # object - oritented Programming, Gabriel Coffey, v0.0
 
 class Person: # Yse PascalCalse for ClassNames 
-    
+    def   __init__(self, name, age, weight):
+        self.name = name
+        self.age = age
+        self.weight = weight
+
+    #
+    def __str__(self):
+        return f"Name: {self.name}\nThis persson is {self.age} years olf\nThey weigh {self.weight} pounds.\n"
+
+
+
+person1 = Person("Nash", 25, 160.1)
+print(person1)        
