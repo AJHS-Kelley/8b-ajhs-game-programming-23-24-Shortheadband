06a68118c8089b217924d488c1f296743b9f86e1
Shortheadband <142345256+Shortheadband@users.noreply.github.com>
2023-12-12 14:04:40 -0500

    ver update
    added comparisons

--- gameProgramming/06_OOP/oop.py
+++ gameProgramming/06_OOP/oop.py
@@ -8,9 +8,25 @@ class Person: # Yse PascalCalse for ClassNames
 
     #
     def __str__(self):
-        return f"Name: {self.name}\nThis persson is {self.age} years olf\nThey weigh {self.weight} pounds.\n"
+        return f"Name: {self.name}\nThis persson is {self.age} years old\nThey weigh {self.weight} pounds.\n"
 
 
 
 person1 = Person("Nash", 25, 160.1)
-print(person1)        
+person2 = Person("Eli", 27, 154.8)
+print(person1)
+print(person2)
+
+if person1.weight > person2.weight:
+    print(f"{person1.name} weighs more than {person2.name}.")
+elif person1.weight == person2.weight:
+    print("Each person weighs the same.\n")
+else:
+    print(f"{person2.name} weighs more than {person1.name}.")
+
+if person1.age > person2.age:
+    print(f"{person1.name} is older than {person2.name}.")
+elif person1.age == person2.age:
+    print("Each person are the same age.\n")
+else:
+    print(f"{person2.name} is older than {person1.name}.")
