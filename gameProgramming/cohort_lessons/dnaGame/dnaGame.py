6567b4dcc16bedbf6b2d11827f4a32d61e9bd74f
Shortheadband <142345256+Shortheadband@users.noreply.github.com>
2024-01-11 14:00:46 -0500

    dnaGame created
    dnaGame created

---
+++ gameProgramming/cohort_lessons/dnaGame/dnaGame.py
@@ -0,0 +1,9 @@
+# DNA relpication Game, gabriel Coffey, v0.0
+
+# Import Entire Modules -- Get whole tool box.
+import time, datetime
+
+# Import Specfic Methods -- Get the specfic tool.
+from random import choice
+
+
