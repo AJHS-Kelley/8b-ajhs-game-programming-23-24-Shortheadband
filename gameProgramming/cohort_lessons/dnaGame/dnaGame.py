d0e119eb7fd6fc901a571b6aa454681a99cee5c8
Shortheadband <142345256+Shortheadband@users.noreply.github.com>
2024-01-11 14:25:04 -0500

    DNA relpication Game, v0.1
    ver update

--- gameProgramming/cohort_lessons/dnaGame/dnaGame.py
+++ gameProgramming/cohort_lessons/dnaGame/dnaGame.py
@@ -1,4 +1,4 @@
-# DNA relpication Game, gabriel Coffey, v0.0
+# DNA relpication Game, gabriel Coffey, v0.1
 
 # Import Entire Modules -- Get whole tool box.
 import time, datetime
@@ -6,4 +6,24 @@ import time, datetime
 # Import Specfic Methods -- Get the specfic tool.
 from random import choice
 
+# Store the DNA Bases
+dnaBases = ["A", "T", "G", "C"]
 
+# Game Functions
+def gameIntro() -> None:
+    pass
+
+def genDNA() -> str:
+    basesGenerated = 0
+    BasesRequested = int(input("Please enter a postive integer number of bases to generate.\n"))
+    dnaSequence = ""
+
+    while basesGenerated < BasesRequested:
+        dnaSequence += choice(dnaBases)
+        basesGenerated += 1
+    return dnaSequence    
+
+dna = genDNA()
+print(dna)
+    
+    #return "STRING"
