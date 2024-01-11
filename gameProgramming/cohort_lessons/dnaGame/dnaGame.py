5f8e674edc2dd9df41322f1537d8b5770d3263de
Shortheadband <142345256+Shortheadband@users.noreply.github.com>
2024-01-11 14:47:34 -0500

    v0.2
    added genRNA

--- gameProgramming/cohort_lessons/dnaGame/dnaGame.py
+++ gameProgramming/cohort_lessons/dnaGame/dnaGame.py
@@ -1,4 +1,4 @@
-# DNA relpication Game, gabriel Coffey, v0.1
+# DNA relpication Game, gabriel Coffey, v0.2
 
 # Import Entire Modules -- Get whole tool box.
 import time, datetime
@@ -27,3 +27,20 @@ dna = genDNA()
 print(dna)
     
     #return "STRING"
+
+def genRNA(dnaSequence: str) -> tuple:
+    print(f"The DNA Sequence is {dnaSequence}.\n")
+    print("You will now genrerate the RNA sequence that would match.\n")
+    print("Please remeber, in the RNA sequence ? matches with A from the DNA sequence")
+    rnaStart = time.time() # time.time() returns the number of seconds since 00:00:00 UTC Jan, 01, 1970
+    rnaSequence = input("Please enter the matching RNA sequence. Leave no spaces! Then press enter.\n").upper()
+    rnaStop = time.time()
+    rnaTime = rnaStop - rnaStart
+
+    return (rnaSequence, rnaTime)
+    # Tuples are ordered -- you can refernce items with the index.
+    # Tuples are UNCHANGEABLE -- you cannot add, modify, or delete after creating
+    # Tuples CAN have duplicate values.
+
+rna = genRNA(dna)
+print(rna)
