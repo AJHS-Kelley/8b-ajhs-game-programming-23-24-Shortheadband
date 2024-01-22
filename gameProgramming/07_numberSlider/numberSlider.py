913d02b7ed8004f827a8dcc70be785ccd2055a0f
Shortheadband <142345256+Shortheadband@users.noreply.github.com>
2024-01-22 13:59:03 -0500

    ver update
    added foundation

---
+++ gameProgramming/07_numberSlider/numberSlider.py
@@ -0,0 +1,20 @@
+# Number Slider, by Gabriel Coffey, based on a project by Al Swelgart, v0.0
+
+import sys, random, pygame
+# sys module provides access to system resources (i.e. Operating system commands)
+
+from pygame.locals import *
+# allows us to call functions from pygame using just the function name instead of module.functions
+# Example: We can use draw() instead of pygame.draw()
+
+# Constants for the Game Board
+BOARDWIDTH = 4 # COLLUMS
+BOARDHEIGHT = 4 # ROWS
+TILESIZE = 80 # MEASURED IN PIXELS
+WINDOWWIDTH = 640 # MEASURED IN PIXELS
+WINDOWHEIGHT = 480 # MEASURED IN PIXELS
+FPS = 30
+BLANK = none
+
+
+
