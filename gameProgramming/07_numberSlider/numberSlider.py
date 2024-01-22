5b923eaf8ca966ad54ef911bbc932f204bab698b
Shortheadband <142345256+Shortheadband@users.noreply.github.com>
2024-01-22 14:04:12 -0500

    Ver update
    added colorvalues

--- gameProgramming/07_numberSlider/numberSlider.py
+++ gameProgramming/07_numberSlider/numberSlider.py
@@ -13,8 +13,14 @@ BOARDHEIGHT = 4 # ROWS
 TILESIZE = 80 # MEASURED IN PIXELS
 WINDOWWIDTH = 640 # MEASURED IN PIXELS
 WINDOWHEIGHT = 480 # MEASURED IN PIXELS
-FPS = 30
+FPS = 30 # This is a maximum value! Don't make a slow computer run faster.
 BLANK = none
 
-
+# COLOR VALUES in (R, G, B) format.
+# 0 = no Value, 255 = Max Value
+BLACK = (0, 0, 0)
+White = (255, 255, 255)
+BRIGHTBLUE = (0, 50, 255)
+DARKTURQUOISE = (3, 54, 73)
+GREEN = (0, 204, 0)
 
