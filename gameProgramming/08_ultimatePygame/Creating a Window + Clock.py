2f5a14a866627d57a67d188c1da4b4585423d0d5
Shortheadband <142345256+Shortheadband@users.noreply.github.com>
2024-02-01 14:09:24 -0500

    Ver update
    Added updated sfx file, added font file, and added text.

--- gameProgramming/08_ultimatePygame/Creating a Window + Clock.py
+++ gameProgramming/08_ultimatePygame/Creating a Window + Clock.py
@@ -3,11 +3,13 @@ from sys import exit
 
 pygame.init()
 screen = pygame.display.set_mode((800,400))
-pygame.display.set_caption('Runner')
+pygame.display.set_caption('SSR')
 clock = pygame.time.Clock()
+test_font = pygame.font.Font('font/Pixeltype.ttf', 50)
 
 sky_surface = pygame.image.load('img/Ult_pygame/Sky.jpg')
 ground_surface = pygame.image.load('img/Ult_pygame/Ground.jpg')
+text_surface = test_font.render('Sunset Runner', False, 'Blue')
 
 
 while True:
@@ -17,7 +19,8 @@ while True:
             exit()
 
     screen.blit(ground_surface,(0,0))
-    screen.blit(sky_surface,(0,0))
+    screen.blit(sky_surface,(0,-25))
+    screen.blit(text_surface,(275,50))
 
     pygame.display.update()
     clock.tick(60)
