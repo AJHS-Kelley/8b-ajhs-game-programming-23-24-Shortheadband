1cf95d97c60dc88b0a8e01d18c88f7751e1750be
Shortheadband <142345256+Shortheadband@users.noreply.github.com>
2024-02-13 11:10:38 -0500

    ver update
    added collision

--- gameProgramming/08_ultimatePygame/Creating a Window + Clock.py
+++ gameProgramming/08_ultimatePygame/Creating a Window + Clock.py
@@ -15,7 +15,7 @@ player_surface = pygame.image.load('img/Ult_pygame/Player/player_walk_1.png')
 player_rectangle = player_surface.get_rect(midbottom = (80,355))
 
 snail_surface = pygame.image.load('img/Ult_pygame/snail/snail1.png').convert_alpha()
-snail_x_pos = 600
+snail_x_rectangle = snail_surface.get_rect(bottomright = (700,355))
 
 
 while True:
@@ -27,10 +27,15 @@ while True:
     screen.blit(ground_surface,(0,0))
     screen.blit(sky_surface,(0,-35))
     screen.blit(text_surface,(275,50))
-    snail_x_pos += -2.5
-    if snail_x_pos < -100: snail_x_pos = 800
+
+    snail_x_rectangle.x -= 3
+    if snail_x_rectangle.right <=0: snail_x_rectangle.left = 800
+
     screen.blit(player_surface,player_rectangle)
-    screen.blit(snail_surface,(snail_x_pos,320))
+    screen.blit(snail_surface,snail_x_rectangle)
+
+    if player_rectangle.colliderect(snail_x_rectangle):
+        print('collision')
 
     pygame.display.update()
     clock.tick(60)
