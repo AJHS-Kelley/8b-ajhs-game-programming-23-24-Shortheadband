091956a50075422cb569c65d934720e842eb3fc9
Shortheadband <142345256+Shortheadband@users.noreply.github.com>
2024-03-27 14:52:11 -0400

    FinalPorject update
    added more code

--- gameProgramming/09_finalProject/finalProject.py
+++ gameProgramming/09_finalProject/finalProject.py
@@ -1,1 +1,28 @@
 # FinalProject v0.0 by Gabriel Coffey
+import sys, random, pygame
+
+resoultion = 0 # 0 = LRes(800,600) 1 = HRes(1920,1080)
+
+if resoultion == 0:
+    X = 800
+    y = 600
+elif resoultion == 1:
+    x = 1920
+    y = 1080
+else: print("This resoultion isn't available")  
+
+pygame.init()
+
+difficulty = int(input("Please choose a difficulty. Enter 1 for Normal or 2 for Encore"))
+
+if difficulty == 1:
+    pygame.display.set_caption('Normal')
+else:    
+    pygame.display.set_caption('Encore')
+
+screen = pygame.display.set_model((x,y))
+
+
+
+
+
