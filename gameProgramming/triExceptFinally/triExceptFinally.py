138238c1118506b3093e890f30aa4f34030e7c7d
Shortheadband <142345256+Shortheadband@users.noreply.github.com>
2024-04-23 13:47:44 -0400

    New example
    triExceptfinally added

---
+++ gameProgramming/triExceptFinally/triExceptFinally.py
@@ -0,0 +1,17 @@
+# This is my method for testing code and preventing crashes.
+# try -- except -- else -- finally
+
+try: # The code in this block is ALWAYS executed
+    myVariable = 1
+    print(myVariabl)
+    
+except NameError: # This code will run If there Is an error(exception)
+    print("There is an incorrect variable name in your code")
+except: 
+    print("Something else went wrong")
+else: # runs if no errors
+    print('Code executed correctly with no exceptions.\n')
+finally: # Always runs
+    print("I'll be back.\n")        
+#except NameError:
+#    pass
