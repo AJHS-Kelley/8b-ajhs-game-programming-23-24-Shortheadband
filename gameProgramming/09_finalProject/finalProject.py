11643cf9ad86e8b501198269ec5da6316e47d41c
Shortheadband <142345256+Shortheadband@users.noreply.github.com>
2024-05-01 13:52:19 -0400

    Issues fixed, New Issue identified
    Fixed -- 3 Errors(Crash code)
    New -- Placeholder Square and Charter sprite both appear
    Old -- Background dosen't fill screen

--- gameProgramming/09_finalProject/finalProject.py
+++ gameProgramming/09_finalProject/finalProject.py
@@ -2,7 +2,7 @@
 
 # Known Issue List:
 #-- Background dosen't fill screen
-#-- 3 New Errors(Crash code)
+#-- Placeholder Square and Charter sprite both appear
 
 # Discarded code:
 
@@ -20,8 +20,9 @@
 #        retryRes
 
 # Debugging Logs
-logFile = "geometryBlasterDebugLog.txt"
+logFile = "finalProjectDebugLog.txt"
 logData = open(logFile, "w") # w = overwrite, a = Append, x = crash
+
 
 from ast import Return
 import sys, random, math, pygame
@@ -66,7 +67,7 @@ def flip(sprites):
 
 def load_sprite_sheets(dir1, dir2, width, height, direction=False):
     path = join("assets", dir1, dir2)
-    images = [f for f in listdir(path) if isfile(join(path, f))] #Error
+    images = [f for f in listdir(path) if isfile(join(path, f))]
 
     all_sprites = {}
 
@@ -81,15 +82,15 @@ def load_sprite_sheets(dir1, dir2, width, height, direction=False):
             sprites.append(pygame.transform.scale2x(surface))
         
         if direction:
-            all_sprites[image.replace(".png," "") + "_right"] = sprites # Error
-            all_sprites[image.replace(".png," "") + "_left"] = flip(sprites)
+            all_sprites[image.replace(".png", "") + "_right"] = sprites
+            all_sprites[image.replace(".png", "") + "_left"] = flip(sprites)
         else:
             all_sprites[image.replace(".png", "")] = sprites
     
     return all_sprites 
 
-
-class Player(pygame.sprite.Sprite): #Error
+#-- Placeholder Square and Charter sprite both appear
+class Player(pygame.sprite.Sprite):
     COLOR = (255,0, 9)
     GRAVITY = 1
     SPRITES = load_sprite_sheets("MainCharacters", "MaskDude", 32, 32, True) 
